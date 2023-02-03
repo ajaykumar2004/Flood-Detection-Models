043281783047b43b030fff08b830e235c87184ce
AjayKumar <91714785+ajaykumar2004@users.noreply.github.com>
2023-02-03 16:06:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def returnPred():
     w = [[float(x) for x in inp.split(",")]]
     
     
-    loaded1=joblib.load('./model_tree.joblib')
-    # loaded2=joblib.load('./model_xgb.joblib') 
-    loaded3=joblib.load('./model_random_forest.joblib') 
-    loaded4=joblib.load('./model_logistic_regression.joblib')
+    loaded1=joblib.load('model_tree.joblib')
+    # loaded2=joblib.load('model_xgb.joblib') 
+    loaded3=joblib.load('model_random_forest.joblib') 
+    loaded4=joblib.load('model_logistic_regression.joblib')
 
     
     d['output_tree'] =str(loaded1.predict(w))
