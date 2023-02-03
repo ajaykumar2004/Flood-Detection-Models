2e26f450744bcbd04eb54dee5b263261dc28db38
AjayKumar <91714785+ajaykumar2004@users.noreply.github.com>
2023-02-03 15:36:14 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route('/api',methods=['GET'])
+def returnPred():
+    d = {}
+    
+    inp  = str(request.args['query'])
+    
+    w = [[float(x) for x in inp.split(",")]]
+    
+    
+    loaded1=joblib.load('lib/kerala/model_tree.joblib')
+    # loaded2=joblib.load('lib/kerala/model_xgb.joblib') 
+    loaded3=joblib.load('lib/kerala/model_random_forest.joblib') 
+    loaded4=joblib.load('lib/kerala/model_logistic_regression.joblib')
+
+    
+    d['output_tree'] =str(loaded1.predict(w))
+    # d['output_xgb'] =str(loaded2.predict(w))
+    d['output_random_forest'] =str(loaded3.predict(w))
+    d['output_logistic_regression'] =str(loaded4.predict(w))
+    return d
+
+if __name__ =='__main__':
+    app.run()
